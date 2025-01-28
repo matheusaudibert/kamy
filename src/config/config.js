a886593ec6f6bc2832fe5a9d24a3a4483bf95b61
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-01-28 20:04:49 -0300

    finish

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,7 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-    API_URL: process.env.API_URL,
-    BOT_TOKEN: process.env.BOT_TOKEN,
-    IMGUR_TOKEN: process.env.IMGUR_TOKEN,
-    CLIENT_ID: process.env.CLIENT_ID
+  API_URL: process.env.API_URL,
+  BOT_TOKEN: process.env.BOT_TOKEN,
+  CLIENT_ID: process.env.CLIENT_ID,
 };
